a66bde7822a107fec87b3954f43577ea9aa82522
msan19 <msan19@student.aau.dk>
2021-10-28 10:29:16 +0200

    Refactor nn_agent (obs, vector is normalized here!)

--- rebound_test/agent/nn_agent.py
+++ rebound_test/agent/nn_agent.py
@@ -1,25 +1,37 @@
 from agent.agent_base import AgentBase
 import tensorflow.keras as keras
+import tensorflow as tf
+import numpy as np
+import time
+from settings.SettingsAccess import settings 
 
 class NNAgent(AgentBase):
 
+    def __init__(self, target_pos, save_path):
+        super().__init__(target_pos)
+        self.model = keras.models.load_model(save_path, compile=False)
+        # tf.compat.v1.disable_eager_execution()
+        self.time = -1
+        self.output = np.array([])
+
     def _get_agent_acceleration(self, sim):
-
-        def __init__(self, target_pos, save_path):
-            self.super().__init__(target_pos)
-            self.model = keras.models.load_model(save_path)
-
         nn_input_data = [self.target_pos[0], self.target_pos[1]]
 
         for particle in sim.particles:
             nn_input_data.extend([particle.x, particle.y, particle.vx, particle.vy, particle.m])
 
-        return self.model.predict(nn_input_data)[0] + [0] # add 0 as the z-axis
-
+        # start_t = time.time()
+        res = np.append( self.model.predict([nn_input_data])[0], [0] ) # add 0 as the z-axis
+        # print(f"Finished predicting. Time spent: {time.time() - start_t}")
+        return res / np.linalg.norm(res) * settings.max_acceleration
 
     def get_thrust(self, archive):
         sim = archive
-        return self._get_agent_acceleration(sim)
+        # print(f"Get thrust. sim.t: {sim.t}")
 
-        
+        if self.time != sim.t:            
+            self.time = sim.t
+            self.output = self._get_agent_acceleration(sim)
+        return self.output
+       
         
