df4c8eddc4c4b08a0679526b0685d7116a77a819
Jakob <jakob.hyldgaard@gmail.com>
2021-11-03 11:42:05 +0100

    added settings dict to tf_main

--- nn_trainer/tf_main.py
+++ nn_trainer/tf_main.py
@@ -1,85 +1,111 @@
-from numpy.core.arrayprint import format_float_scientific
 import tensorflow as tf
 import tensorflow.keras.layers as layers
-from tensorflow.python.eager.context import graph_mode
 from TFTrainer import TFTrainer
 import nn_util
 from pathlib import Path
 import os
-import numpy as np
-import math
 import time
-from datetime import timedelta
+from tensorflow.keras import activations
+from tensorflow.keras import optimizers
+from tensorflow.keras import losses
+from tensorflow.keras.callbacks import EarlyStopping
+import numpy as np
 
 
 def get_data_dir(): 
-    return Path.joinpath(Path().resolve(), "data")
+    return Path.joinpath(Path().resolve(), settings["data_dir_name"])
 
 def get_data_files(data_dir):
     return os.listdir(data_dir)
 
 if __name__ == '__main__':
+    settings = {
+        "data_in_single_file": True,
+        "data_dir_name": "data",
+        "data_file_name": "02-11-2021_13-20-41.json",
+        "model_save_name": "no_planet_128_32_8",
+        "do_evaluation": True,
+        "do_early_stopping": True,
+        "do_save_model": True,
+        "evaluation_size": 30,
+        "early_stopping_patience": 50,
+        "shuffle_data_in_batch": False,
+    }
+    training_settings = {
+        "bacth_size": 32,
+        "epochs": 10000,
+        "verbose": 2,
+    }
+
     model = tf.keras.models.Sequential()
-    model.add(layers.Dense(17))
-    model.add(layers.Dense(17, activation="sigmoid"))
-    model.add(layers.Dense(17, activation="relu"))
+
+    model.add(layers.Dense(128, activation=activations.relu))
+    model.add(layers.Dense(32, activation=activations.relu))
+    model.add(layers.Dense(8, activation=activations.relu))
     model.add(layers.Dense(2))
     
-    model.compile(optimizer='adam',
-                  loss='mean_squared_error',
-                #   metrics=[tf.keras.metrics.Accuracy()]
+    model.compile(optimizer=optimizers.Adam(learning_rate=1e-3, decay=1e-3 / 200),  # learning_rate=0.1
+                  loss=losses.MAE,
+                  #   metrics=[tf.keras.metrics.Accuracy()]
                   )
 
 
-    data_dir = get_data_dir()
-    data = get_data_files(data_dir)
-    model_name = "full_training_17_17_sigmoid"
-    trainer = TFTrainer(model, model_name)
-    # trainer = TFTrainer.load_model("full_training_32_32/2021_10_27_13_43")
 
-    evaluation_size = 30
-    evaluation_data = data[:evaluation_size]
-    training_data = data[evaluation_size:]
-    
-    evaluate_every = np.floor(len(training_data)/evaluation_size)
+    data_dir = get_data_dir()
+
+    trainer = TFTrainer(model, settings["model_save_name"])
+
+
+    if (settings["do_early_stopping"]):
+        es = EarlyStopping(monitor='loss', mode='min', verbose=2, patience=settings["early_stopping_patience"])
+        trainer.cp_callbacks.append(es)
 
     start_time = time.time()
-    for file_index in range(len(training_data)):
-        file = data[file_index]
+    
 
-        path_to_json_file = str( Path.joinpath( data_dir, file ) )
-        # print(f" data: {path_to_json_file}")
-        print(f"Training file: {file_index}/{len(training_data)}", end="\r")
+    if (settings["data_in_single_file"]):
+        path_to_json_file = Path.joinpath(data_dir, settings["data_file_name"])
+        X, y = nn_util.load_nn_data(path_to_json_file)
+        trainer.fit(X, y, batch_size=training_settings["bacth_size"], epochs=training_settings["epochs"], verbose=training_settings["verbose"])
+    else:
+        if (settings["do_evaluation"]):
+            data = get_data_files(data_dir)
+            evaluation_data = data[:settings["evaluation_size"]]
+            training_data = data[settings["evaluation_size"]:]
+            evaluate_every = np.floor(len(training_data)/settings["evaluation_size"])
 
+        for file_index in range(len(training_data)):
+            file = data[file_index]
 
-        X, y = nn_util.load_nn_data(path_to_json_file, 17, 2)
-
-        shuffled_indexes = [i for i in range(len(X))]
-        
-        X = [X[i] for i in shuffled_indexes]
-        y = [y[i] for i in shuffled_indexes]
-
-
-        trainer.fit(X, y, batch_size=32, epochs=15, verbose=0)
-        
-        if file_index != 0 and file_index % (len(training_data)//evaluation_size) == 0:
-            eval_index = math.floor(evaluation_size*(file_index/len(training_data)))
-            
-            file = evaluation_data[eval_index]
             path_to_json_file = str( Path.joinpath( data_dir, file ) )
-            eval_X, eval_y = nn_util.load_nn_data(path_to_json_file, 17, 2)
+            # print(f" data: {path_to_json_file}")
+            print(f"Training file: {file_index}/{len(training_data)}", end="\r")
 
-            print(f"eval {eval_index}/{evaluation_size}:", end="\n")
-            trainer.evaluate(eval_X, eval_y, batch_size=32)
-    
+
+            X, y = nn_util.load_nn_data(path_to_json_file)
+
+            if (settings["shuffle_data_in_batch"]):
+                shuffled_indexes = [i for i in range(len(X))]
+
+                X = [X[i] for i in shuffled_indexes]
+                y = [y[i] for i in shuffled_indexes]
+
+            trainer.fit(X, y, batch_size=training_settings["bacth_size"], epochs=training_settings["epochs"], verbose=training_settings["verbose"])
+            
+            if (settings["do_evaluation"]):
+                if file_index != 0 and file_index % (len(training_data)//settings["evaluation_size"]) == 0:
+                    eval_index = np.floor(settings["evaluation_size"]*(file_index/len(training_data)))
+                    
+                    file = evaluation_data[eval_index]
+                    path_to_json_file = str( Path.joinpath( data_dir, file ) )
+                    eval_X, eval_y = nn_util.load_nn_data(path_to_json_file, 17, 2)
+
+                    print(f"eval {eval_index}/{settings['evaluation_size']}:", end="\n")
+                    trainer.evaluate(eval_X, eval_y, batch_size=training_settings["bacth_size"])
+                    
+
     print("Done Training!")
-    print(f"Time spent: { str( timedelta( seconds=time.time()-start_time ) ) }")
-    trainer.evaluate(X, y, batch_size=32)
-    trainer.save_model()
-    
-        # trainer2 = TFTrainer.load_model_from_checkpoint(model, model_name)
-        # print(trainer2)
+    # print(f"Time spent: { str( timedelta( seconds=time.time()-start_time ) ) }")
 
-    # trainer.save_model()
-    # print(predict_y, y[0])
-
+    if (settings["do_save_model"]):
+        trainer.save_model()
