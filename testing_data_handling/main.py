afdd2d0f6ab09255371ca87d4d748178ef226229
MLServer1101 <93983002+MLServer1101@users.noreply.github.com>
2021-12-03 07:53:56 +0100

    gcpd testing

--- testing_data_handling/main.py
+++ testing_data_handling/main.py
@@ -7,17 +7,14 @@ import json
 from matplotlib import cm
 import file_util as FileHandler
 from pathlib import Path
-from progresbar import *
+from progress.bar import IncrementalBar
 from scipy import stats
 import pandas as pd
 
-
 def extract_data():
-    num_of_environments = 1000
+    data_dir = FileHandler.get_data_dir("testing_data")
 
-    data_dir = FileHandler.get_data_dir("testing_data_handling/testing_data")
-
-    data = [dir for dir in FileHandler.get_data_files(data_dir) if "nn_grav" in dir ]
+    data = [dir for dir in FileHandler.get_data_files(data_dir)]
 
     processed_data = {}
 
@@ -34,12 +31,25 @@ def extract_data():
             "collisions": [],
             "reaches_target": [],
             "stays_at_target": [],
-            "fuel_for_at_target_at_end": []
+            "fuel_for_at_target_at_end": [],
+            "acc_se": [],
+            "acc_ae": [],
+            "capped_acc_se": [],
+            "capped_acc_ae": [],
+            "grav_length": [],
+            "grav_length_reach_target": [],
+            "grav_length_stay_at_target": [],
+            "max_grav": [],
+            "max_grav_stays_at_target": []
         }
 
+        folder = Path.joinpath(data_dir, model)
+
+        num_of_environments = int(len(FileHandler.get_data_files(folder))/2)
+
+        bar = IncrementalBar('File loaded: ', max=num_of_environments, suffix='%(percent)d%%')
+
         for i in range(num_of_environments):
-              
-            folder = Path.joinpath(data_dir, model)
 
             archive_path = str(folder) + "/archive_" + str(i) + ".json"
 
@@ -63,16 +73,31 @@ def extract_data():
 
                 at_target_at_end = dist_to_target[-1] < dist_to_target[0] * 0.05
                 extracted_data["stays_at_target"].append(1 if at_target_at_end else 0)
+
+                grav_lengths = []
+                for vec in metrics["grav_acceleration"]:
+                    grav_lengths.append(np.linalg.norm(vec))
+                extracted_data["grav_length"].extend(grav_lengths)
+
+                max_grav = max(grav_lengths)
+
+                extracted_data["max_grav"].append(max_grav)
+
                 if at_target_at_end :
                     extracted_data["fuel_for_at_target_at_end"].append(agent_fuel)
+                    extracted_data["grav_length_stay_at_target"].extend(grav_lengths)
+                    extracted_data["max_grav_stays_at_target"].append(max_grav)
 
                 agent_fuel = 0
                 target_reached = 0
+                grav_lengths = []
                 for i in range(len(dist_to_target)):
                     agent_fuel += min(10, np.linalg.norm(metrics["agent_acceleration"][i]))
+                    grav_lengths.append(np.linalg.norm(metrics["grav_acceleration"][i]))
                     if dist_to_target[i] < dist_to_target[0] * 0.05:
                         extracted_data["time_to_5p_to_target"].append(i * 0.01)
                         extracted_data["fuel_to_5p_to_target"].append(agent_fuel)
+                        extracted_data["grav_length_reach_target"].extend(grav_lengths)
                         target_reached = 1
                         break
                 extracted_data["reaches_target"].append(target_reached)
@@ -80,9 +105,22 @@ def extract_data():
                 extracted_data["end_cost"].append(dist_to_target[-1])
 
 
+                for i in range(len(metrics["agent_acceleration"])):
+                    model_to_gcpd_diff = np.array(metrics["agent_acceleration"][i]) - np.array(metrics["gcpd_acceleration"][i])
+                    capped_model_to_gcpd_diff = np.array(cap_vector_length(metrics["agent_acceleration"][i], 10)) - np.array(metrics["gcpd_acceleration"][i])
+
+                    for value in capped_model_to_gcpd_diff[:2]:
+                        extracted_data["capped_acc_se"].append(value**2)
+                        extracted_data["capped_acc_ae"].append(abs(value))
+
+                    for value in model_to_gcpd_diff[:2]:
+                        extracted_data["acc_se"].append(value**2)
+                        extracted_data["acc_ae"].append(abs(value))
+
+                ...
+
             bar.next()
         print( f"\t{model} " + " " * (30 - len(model)) + "has been loaded")
-        resetBar()
 
         processed_data[model] = {}
 
@@ -91,9 +129,21 @@ def extract_data():
             processed_data[model][key + "_m"] = float(m)
             processed_data[model][key + "_h"] = float(h)
 
-    with open("testing_data_handling/extracted_data.json", "w") as file:
+    with open("extracted_data.json", "w") as file:
             jsonstr = json.dumps(processed_data, indent=4)
             file.write(jsonstr)
+
+def abs_vector_pr_dim(vec):
+    sum = 0
+    for value in vec:
+        sum += abs(value)
+    return sum
+
+def cap_vector_length(vec, max_length):
+    length = np.linalg.norm(vec)
+    if length > max_length:
+        return vec / length * max_length
+    return vec
 
 def mean_confidence_interval(data, confidence=0.95):
     """Stolen from stack-overflow"""
@@ -104,10 +154,10 @@ def mean_confidence_interval(data, confidence=0.95):
 
     return m, h
 
-def plot_data_2d(data_key, label = "y", min = 0, max = 1000):
+def plot_data_2d(data_key, title = "", label = "y", min = 0, max = 1000):
 
     data = None
-    with open("testing_data_handling/extracted_data.json", "r") as file:
+    with open("extracted_data.json", "r") as file:
                 json_file = file.read()
                 data = json.loads(json_file)
 
@@ -136,43 +186,63 @@ def plot_data_2d(data_key, label = "y", min = 0, max = 1000):
     plt.ylabel(label)
     plt.xlabel("Model")
 
+    plt.title(title)
+
     plt.show()
 
 def fuel_to_5p_to_target_plot():
-    plot_data_2d("fuel_to_5p_to_target", label = "Fuel", min = 18000, max = 22000)
+    plot_data_2d("fuel_to_5p_to_target", title="Fuel to reach target", label = "Fuel", min = 17000, max = 21000)
 
 def fuel_plot():
-    plot_data_2d("fuel", label = "Fuel", min = 25000, max = 35000)
+    plot_data_2d("fuel", title="Fuel", label = "Fuel", min = 25000, max = 30000)
 
 def reaches_target_plot():
-    plot_data_2d("reaches_target", label = "Rate of Reaching Target", min = 0.5, max = 0.9)
+    plot_data_2d("reaches_target", title="Rate of reaching target", label = "Rate", min = 0, max = 1)
 
 def collisions_plot():
-    plot_data_2d("collisions", label = "Rate of Colliding", min = 0.0, max = 0.3)
+    plot_data_2d("collisions", title="Rate of collision", label = "Rate", min = 0.0, max = 1)
 
 def agent_time_plot():
-    plot_data_2d("agent_time", label = "Time", min = 0, max = 0.01)
+    plot_data_2d("agent_time", title="Time of neural network computations", label = "Time", min = 0, max = 0.002)
 
 def gcpd_time_plot():
-    plot_data_2d("gcpd_time", label = "Time", min = 0, max = 0.001)
+    plot_data_2d("gcpd_time", title="Time of GCPD computations", label = "Time", min = 0, max = 0.0002)
 
 def overhead_time_plot():
-    plot_data_2d("overhead_time", label = "Time", min = 0, max = 0.00-1)
+    plot_data_2d("overhead_time", title="Time of overhead computations", label = "Time", min = 0, max = 0.0002)
 
 def end_cost_plot():
-    plot_data_2d("end_cost", label = "Cost", min = 0, max = 40000)
+    plot_data_2d("end_cost", title="Cost given as final distance to target", label = "Cost", min = 0, max = 30000)
 
 def time_to_5p_to_target_plot():
-    plot_data_2d("time_to_5p_to_target", label = "Time", min = 0, max = 25)
+    plot_data_2d("time_to_5p_to_target", title="Time to reach target", label = "Time", min = 20, max = 22)
 
 def stays_at_target_plot():
-    plot_data_2d("stays_at_target", label = "Rate of Staying at Target", min = 0.4, max = 0.65)
+    plot_data_2d("stays_at_target", title="Rate of staying at target", label = "Rate", min = 0, max = 1)
 
 def fuel_for_at_target_at_end_plot():
-    plot_data_2d("fuel_for_at_target_at_end", label = "Fuel", min = 20000, max = 30000)
+    plot_data_2d("fuel_for_at_target_at_end", title="Fuel to reach and stay at target", label = "Fuel", min = 20000, max = 30000)
+
+def abs_loss_plot():
+    plot_data_2d("acc_abs_loss", title="Loss between model and GCPD (mae)", label = "Loss", min = 0, max = 5)
+
+def square_loss_plot():
+    plot_data_2d("acc_diff", title="Loss between model and GCPD (mse)", label = "Loss", min = 0, max = 5)
 
 if __name__ == "__main__":
     #extract_data()
-    
+    # fuel_to_5p_to_target_plot()
+    # fuel_plot()
+    # reaches_target_plot()
+    # collisions_plot()
+    # agent_time_plot()
+    # gcpd_time_plot()
+    # overhead_time_plot()
+    end_cost_plot()
+    # time_to_5p_to_target_plot()
+    # stays_at_target_plot()
+    #fuel_for_at_target_at_end_plot()
+    #abs_loss_plot()
+    # square_loss_plot()
 
     ...
